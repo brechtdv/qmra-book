7a096b05c3f60a41867fb1bcaca811de416447de
Brecht Devleesschauwer <Brecht.Devleesschauwer@UGent.be>
2016-02-06 15:55:00 -0500

    remove bullets

--- _index/home.php
+++ _index/home.php
@@ -3,7 +3,7 @@
 <p>This is the course site for <strong>“Quantitative Food Safety”</strong>.</p>
 <p>Something on quantitative burden and risk assessment.</p>
 
-<ul class="toc">
+<ul class="toc" style="list-style-type: none;">
 <?php
 readfile("https://raw.githubusercontent.com/brechtdv/qmra-book/master/_index/nav.html");
 ?>
